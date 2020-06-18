e2982b813158a3db341dc766a559d2ea1404d982
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2020-06-18 14:09:34 +0530

    rules display ready

--- src/rules_display.js
+++ src/rules_display.js
@@ -2,11 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class RulesDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+     
+        this.state = {
+          data: null,
+        };
+    }
+
+    componentDidMount() {
+        fetch('/translation_rules.json')
+          .then(response => response.json())
+          .then(data => this.setState({ data }));
+    }
+
     render() {
+       if(this.state.data!==null) {
+           return(
+               <ul>
+                   {
+                       Object.keys(this.state.data).map((val, idx) => {
+                       return(<li>{val} -> {this.state.data[val].join(", ")}</li>);
+                       })
+                   }
+               </ul>
+           );
+       }
        return (
-          <div>
-             <h1>Contact...</h1>
-          </div>
+        <div className="text-center my-5">
+            <h1>Hindi - Awadhi Translation Rules</h1>
+        </div>
        )
     }
  }
