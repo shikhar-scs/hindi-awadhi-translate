b840ca4ff169ea776b39dc16da6447f552266a67
shikhar-scs <shikhar9a@gmail.com>
2020-06-19 14:58:00 +0530

    Updates

---
+++ precache-manifest.0d47ca954dcd988547bc83f17e34a548.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531033b6258a8b70f229a1d66ba24ee1",
+    "url": "/hindi-awadhi-translate/index.html"
+  },
+  {
+    "revision": "71a14b7d61d8c3b3d9b8",
+    "url": "/hindi-awadhi-translate/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4b648105ded8351c21bb",
+    "url": "/hindi-awadhi-translate/static/css/main.5cfdc261.chunk.css"
+  },
+  {
+    "revision": "71a14b7d61d8c3b3d9b8",
+    "url": "/hindi-awadhi-translate/static/js/2.97b4076e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hindi-awadhi-translate/static/js/2.97b4076e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b648105ded8351c21bb",
+    "url": "/hindi-awadhi-translate/static/js/main.8e9eaeaf.chunk.js"
+  },
+  {
+    "revision": "577ba5d0377be5e17d5c",
+    "url": "/hindi-awadhi-translate/static/js/runtime-main.80ddb6d2.js"
+  }
+]);
